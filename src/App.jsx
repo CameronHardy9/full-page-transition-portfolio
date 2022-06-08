15deb3b19837095c773f33e0bf5af77b68cbdcea
Cameron Hardy <camhardy99@gmail.com>
2022-06-08 16:41:06 -0400

    Restructured main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Home from './components/Home';
-import About from './components/About'
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import { Routes, Route } from 'react-router-dom'
+import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 export default function App() {
-    const backgroundColor = (color) => {
-        document.querySelector('body').style.backgroundColor = color
-    };
-
     return (
-        <Routes>
-            <Route path='/' element={<Home backgroundColor={backgroundColor} />} />
-            <Route path='/about' element={<About backgroundColor={backgroundColor} />} />
-            <Route path='/projects' element={<Projects backgroundColor={backgroundColor} />} />
-            <Route path='/contact' element={<Contact backgroundColor={backgroundColor} />} />
-            <Route path='/*' element={<h1>Page Not Found!</h1>} />
-        </Routes>
+        <>
+            <header>
+                <Nav />
+            </header>
+            <main>
+                <Home />
+                <About />
+                <Projects />
+                <Contact />
+            </main>
+        </>
     );
 };
