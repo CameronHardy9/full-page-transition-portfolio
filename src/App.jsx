0531cf06fa14a617b14f3fea9c74b4df3be00a29
Cameron Hardy <camhardy99@gmail.com>
2022-06-09 12:13:58 -0400

    Added state management for view and function for nav to update view

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import Nav from "./components/Nav/Nav";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
+import Nav from "./components/Nav/Nav.jsx";
+import Home from "./components/pages/Home/Home.jsx";
+import About from "./components/pages/About/About.jsx";
+import Projects from "./components/pages/Projects/Projects.jsx";
+import Contact from "./components/pages/Contact/Contact.jsx";
+import { useState } from 'react';
 
 export default function App() {
+
+    const [translateX, setTranslateX] = useState(0);
+    const [translateY, setTranslateY] = useState(0);
+
+
+    function updateView(x, y) {
+        setTranslateX(x);
+        setTranslateY(y);
+    }
+
     return (
         <>
             <header>
-                <Nav />
+                <Nav updateView={updateView} />
             </header>
-            <main className="pageContainer">
+            <main className="pageContainer" style={{transform: `translate(${translateX}%, ${translateY}%)`}}>
                 <Home />
                 <About />
                 <Projects />
