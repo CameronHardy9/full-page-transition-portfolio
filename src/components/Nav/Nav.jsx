00deaa7a5d6202b25f722c1a466167f50aa0e783
Cameron Hardy <camhardy99@gmail.com>
2022-06-09 12:12:22 -0400

    Set up nav to update page view

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,21 +1,27 @@
 import './Nav.css';
 
-export default function Nav() {
-    return(
-        //TODO: Add onClick to each button for CSS page transitions
+export default function Nav(props) {return(
         <nav className="nav">
             <ul>
                 <li>
-                    <button type='button' aria-label='Home'>Home</button>
+                    <button type='button' aria-label='Home' onClick={() => {
+                        props.updateView(0, 0);
+                    }}>Home</button>
                 </li>
                 <li>
-                    <button type='button' aria-label='About'>About</button>
+                    <button type='button' aria-label='About' onClick={() => {
+                        props.updateView(-100, 0);
+                    }}>About</button>
                 </li>
                 <li>
-                    <button type='button' aria-label='Projects'>Projects</button>
+                    <button type='button' aria-label='Projects' onClick={() => {
+                        props.updateView(0, -50);
+                    }}>Projects</button>
                 </li>
                 <li>
-                    <button type='button' aria-label='Contact'>Contact</button>
+                    <button type='button' aria-label='Contact' onClick={() => {
+                        props.updateView(-100, -50);
+                    }}>Contact</button>
                 </li>
             </ul>
         </nav>
