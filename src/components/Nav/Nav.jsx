700b8052d10408406b6497bfeebfc786d814aaa0
Cameron Hardy <camhardy99@gmail.com>
2022-06-10 22:44:53 -0400

    Set new page coordinates for nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,22 +5,22 @@ export default function Nav(props) {return(
             <ul>
                 <li>
                     <button type='button' aria-label='Home' onClick={() => {
-                        props.updateView(0, 0);
+                        props.updateView(-100, -14.2857);
                     }}>Home</button>
                 </li>
                 <li>
                     <button type='button' aria-label='About' onClick={() => {
-                        props.updateView(-100, 0);
+                        props.updateView(-300, 0);
                     }}>About</button>
                 </li>
                 <li>
                     <button type='button' aria-label='Projects' onClick={() => {
-                        props.updateView(0, -50);
+                        props.updateView(0, -71.4285);
                     }}>Projects</button>
                 </li>
                 <li>
                     <button type='button' aria-label='Contact' onClick={() => {
-                        props.updateView(-100, -50);
+                        props.updateView(-500, -57.1428);
                     }}>Contact</button>
                 </li>
             </ul>
