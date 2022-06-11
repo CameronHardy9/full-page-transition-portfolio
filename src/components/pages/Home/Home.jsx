73ad373fe2fbe1c5bccaa866d9aedc5580a33419
Cameron Hardy <camhardy99@gmail.com>
2022-06-10 23:27:16 -0400

    Added sample home screen copy and styling

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -3,7 +3,11 @@ import './Home.css';
 export default function Home(){
     return(
         <section className="page grey">
-            <h1>Home</h1>
+            <h1>
+                <span>Hi</span>
+                <span>I am</span>
+                <span>Cameron</span>
+            </h1>
         </section>
     );
 };
