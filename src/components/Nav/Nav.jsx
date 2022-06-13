54ec14c7719642c16a259760dbb8d914c23611c4
Cameron Hardy <camhardy99@gmail.com>
2022-06-13 13:54:07 -0400

    Refactored to make code easier to update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,26 +1,30 @@
 import './Nav.css';
 
-export default function Nav(props) {return(
+export default function Nav(props) {
+    //For y-axis coordinate - 100 / # of total rows in grid
+    const magicNegativeNumber = -(100 / 7);
+
+    return(
         <nav className="nav">
             <ul>
                 <li>
                     <button type='button' aria-label='Home' onClick={() => {
-                        props.updateView(-100, -14.2857);
+                        props.updateView(-100, magicNegativeNumber);
                     }}>Home</button>
                 </li>
                 <li>
                     <button type='button' aria-label='About' onClick={() => {
-                        props.updateView(-300, 0);
+                        props.updateView(-300, 0 * magicNegativeNumber);
                     }}>About</button>
                 </li>
                 <li>
                     <button type='button' aria-label='Projects' onClick={() => {
-                        props.updateView(0, -71.4285);
+                        props.updateView(0, 5 * magicNegativeNumber);
                     }}>Projects</button>
                 </li>
                 <li>
                     <button type='button' aria-label='Contact' onClick={() => {
-                        props.updateView(-500, -57.1428);
+                        props.updateView(-500, 4 * magicNegativeNumber);
                     }}>Contact</button>
                 </li>
             </ul>
