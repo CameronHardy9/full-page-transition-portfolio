512a3d88b8d3c934d1c3678b1a7da0affcb13cce
Cameron Hardy <camhardy99@gmail.com>
2022-06-13 16:33:31 -0400

    Creted foundation of miniMap functionality

--- src/components/MiniMap/MiniMap.jsx
+++ src/components/MiniMap/MiniMap.jsx
@@ -1,10 +1,29 @@
-import 'MiniMap.css';
+import './MiniMap.css';
+import { useState, useEffect } from 'react';
+import uniqid from 'uniqid';
 
-export default function MiniMap() {
+export default function MiniMap(props) {
+    const [miniMapPages, setMiniMapPages] = useState([]);
+    const [moving, setMoving] = useState(true);
+
+    useEffect(() => {
+        for (let i = 0; i < 49; i++) {
+            setMiniMapPages((prevState) => [
+                ...prevState,
+                <div key={uniqid()} className='miniMapPage'></div>
+            ])
+        }
+        console.log(miniMapPages)
+    }, [])
+
     return(
-        <>
-            {/* Heads-up display for site location 
-            in the overall grid layout */}
-        </>
+        <div className='miniMapContainer' style={{display: `${moving ? 'grid' : 'none'}`}}>
+            <div className='miniMapHead' style={{ top: `calc((100% / 7) * ${props.miniMapY})`, left: `calc((100% / 7) * ${props.miniMapX})`, transition: `all ${props.differentialDelay}s` }}></div>
+            {
+                miniMapPages.map((page) => {
+                    return page;
+                })
+            }
+        </div>
     );
 };
