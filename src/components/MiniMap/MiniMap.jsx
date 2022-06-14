8a5392c1d216ae61707205cafcb06409a233af83
Cameron Hardy <camhardy99@gmail.com>
2022-06-14 13:14:34 -0400

    Removed unused state and logs, updated transition property

--- src/components/MiniMap/MiniMap.jsx
+++ src/components/MiniMap/MiniMap.jsx
@@ -4,7 +4,6 @@ import uniqid from 'uniqid';
 
 export default function MiniMap(props) {
     const [miniMapPages, setMiniMapPages] = useState([]);
-    const [moving, setMoving] = useState(true);
 
     useEffect(() => {
         for (let i = 0; i < 49; i++) {
@@ -13,12 +12,16 @@ export default function MiniMap(props) {
                 <div key={uniqid()} className='miniMapPage'></div>
             ])
         }
-        console.log(miniMapPages)
     }, [])
 
     return(
-        <div className='miniMapContainer' style={{display: `${moving ? 'grid' : 'none'}`}}>
-            <div className='miniMapHead' style={{ top: `calc((100% / 7) * ${props.miniMapY})`, left: `calc((100% / 7) * ${props.miniMapX})`, transition: `all ${props.differentialDelay}s` }}></div>
+        <div className='miniMapContainer' >
+            <div className='miniMapHead' style={{ 
+                    top: `calc((100% / 7) * ${props.miniMapY})`, 
+                    left: `calc((100% / 7) * ${props.miniMapX})`, 
+                    transitionProperty: `top, left`, 
+                    transitionDuration: `${props.differentialDelay}s` 
+                }}></div>
             {
                 miniMapPages.map((page) => {
                     return page;
