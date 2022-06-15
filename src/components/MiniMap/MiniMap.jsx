324248a2e2ce9a7f14fdb4e49c7f04007241197f
Cameron Hardy <camhardy99@gmail.com>
2022-06-14 21:07:30 -0400

    Added slider and button for MiniMap

--- src/components/MiniMap/MiniMap.jsx
+++ src/components/MiniMap/MiniMap.jsx
@@ -4,6 +4,7 @@ import uniqid from 'uniqid';
 
 export default function MiniMap(props) {
     const [miniMapPages, setMiniMapPages] = useState([]);
+    const [show, setShow] = useState(false);
 
     useEffect(() => {
         for (let i = 0; i < 49; i++) {
@@ -15,18 +16,26 @@ export default function MiniMap(props) {
     }, [])
 
     return(
-        <div className='miniMapContainer' >
-            <div className='miniMapHead' style={{ 
-                    top: `calc((100% / 7) * ${props.miniMapY})`, 
-                    left: `calc((100% / 7) * ${props.miniMapX})`, 
-                    transitionProperty: `top, left`, 
-                    transitionDuration: `${props.differentialDelay}s` 
-                }}></div>
-            {
-                miniMapPages.map((page) => {
-                    return page;
-                })
-            }
+        <div className='miniMapSlider' style={{
+            transition: 'left 1s',
+            left: `${show ? 0 : -30}vw`
+        }}>
+            <div className='miniMapGridContainer' >
+                <div className='miniMapHead' style={{ 
+                        top: `calc((100% / 7) * ${props.miniMapY})`, 
+                        left: `calc((100% / 7) * ${props.miniMapX})`, 
+                        transitionProperty: `top, left`, 
+                        transitionDuration: `${props.differentialDelay}s` 
+                    }}></div>
+                {
+                    miniMapPages.map((page) => {
+                        return page;
+                    })
+                }
+            </div>
+            <button className='miniMapSliderButton' onClick={() => {
+                setShow(!show);
+            }}>Mini Map</button>
         </div>
     );
 };
