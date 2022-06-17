9b3b707699f6f4a09d3dc17db8bd3ea258640f4f
Cameron Hardy <camhardy99@gmail.com>
2022-06-17 11:31:20 -0400

    Updated project page with grid of projects

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -3,7 +3,34 @@ import './Projects.css';
 export default function Projects(){
     return(
         <section className="projectsContainer page orange">
-            <h1>Projects</h1>
+            <article className='project'>
+                <h2>Portfolio Site</h2>
+                <div className="projectLinks">
+                    <a href="https://github.com/CameronHardy9/full-page-transition-portfolio" rel="noreferrer" target="_blank">GitHub Repo</a>
+                    <a href="https://www.cameron-hardy.com" rel="noreferrer" target="_blank">Live Site</a>
+                </div>
+            </article>
+            <article className='project'>
+                <h2>Checkbook Pocket</h2>
+                <div className="projectLinks">
+                    <a href="https://github.com/CameronHardy9/CheckBook-Pocket" rel="noreferrer" target="_blank">GitHub Repo</a>
+                    <a href="https://checkbook-pocket.web.app" rel="noreferrer" target="_blank">Live Site</a>
+                </div>
+            </article>
+            <article className='project'>
+                <h2>Kanban Go</h2>
+                <div className="projectLinks">
+                    <a href="https://github.com/CameronHardy9/Kanban_Go" rel="noreferrer" target="_blank">GitHub Repo</a>
+                    <a href="https://kanban-go.netlify.app/" rel="noreferrer" target="_blank">Live Site</a>
+                </div>
+            </article>
+            <article className='project'>
+                <h2>Pokedex</h2>
+                <div className="projectLinks">
+                    <a href="https://github.com/CameronHardy9/Pokedex_Project" rel="noreferrer" target="_blank">GitHub Repo</a>
+                    <a href="https://project-pokedex.netlify.app/" rel="noreferrer" target="_blank">Live Site</a>
+                </div>
+            </article>
         </section>
     );
 };
