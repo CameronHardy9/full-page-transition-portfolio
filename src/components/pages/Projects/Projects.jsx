c35030c676431a30befa281f7ce7d24e498ec293
Cameron Hardy <camhardy99@gmail.com>
2022-06-20 15:18:59 -0400

    Added navigation functionality for project page views

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -1,30 +1,41 @@
 import './Projects.css';
 
-export default function Projects(){
+export default function Projects(props){
+    //For y-axis coordinate - 100 / # of total rows in grid
+    const magicNegativeNumber = -(100 / 7);
+
     return(
         <section className="projectsContainer page orange">
-            <article className='project'>
+            <article onClick={() => {
+                props.updateView(0, 0 * magicNegativeNumber);
+            }} className='project'>
                 <h2>Portfolio Site</h2>
                 <div className="projectLinks">
                     <a href="https://github.com/CameronHardy9/full-page-transition-portfolio" rel="noreferrer" target="_blank">GitHub Repo</a>
                     <a href="https://www.cameron-hardy.com" rel="noreferrer" target="_blank">Live Site</a>
                 </div>
             </article>
-            <article className='project'>
+            <article onClick={() => {
+                props.updateView(-100, 0 * magicNegativeNumber);
+            }} className='project'>
                 <h2>Checkbook Pocket</h2>
                 <div className="projectLinks">
                     <a href="https://github.com/CameronHardy9/CheckBook-Pocket" rel="noreferrer" target="_blank">GitHub Repo</a>
                     <a href="https://checkbook-pocket.web.app" rel="noreferrer" target="_blank">Live Site</a>
                 </div>
             </article>
-            <article className='project'>
+            <article onClick={() => {
+                props.updateView(-200, 0 * magicNegativeNumber);
+            }} className='project'>
                 <h2>Kanban Go</h2>
                 <div className="projectLinks">
                     <a href="https://github.com/CameronHardy9/Kanban_Go" rel="noreferrer" target="_blank">GitHub Repo</a>
                     <a href="https://kanban-go.netlify.app/" rel="noreferrer" target="_blank">Live Site</a>
                 </div>
             </article>
-            <article className='project'>
+            <article onClick={() => {
+                props.updateView(-400, 0 * magicNegativeNumber);
+            }} className='project'>
                 <h2>Pokedex</h2>
                 <div className="projectLinks">
                     <a href="https://github.com/CameronHardy9/Pokedex_Project" rel="noreferrer" target="_blank">GitHub Repo</a>
