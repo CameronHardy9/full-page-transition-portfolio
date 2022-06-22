b5531c21abab73e8bad837945c12d1dccf8e5383
Cameron Hardy <camhardy99@gmail.com>
2022-06-22 14:20:21 -0400

    Refactored page to act as nav with button links for view change

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -6,42 +6,26 @@ export default function Projects(props){
 
     return(
         <section className="projectsContainer page orange">
-            <article onClick={() => {
+            <button className='projectNav' onClick={() => {
                 props.updateView(0, 0 * magicNegativeNumber);
-            }} className='project'>
-                <h2>Portfolio Site</h2>
-                <div className="projectLinks">
-                    <a href="https://github.com/CameronHardy9/full-page-transition-portfolio" rel="noreferrer" target="_blank">GitHub Repo</a>
-                    <a href="https://www.cameron-hardy.com" rel="noreferrer" target="_blank">Live Site</a>
-                </div>
-            </article>
-            <article onClick={() => {
+            }}>
+                Portfolio Site
+            </button>
+            <button className='projectNav' onClick={() => {
                 props.updateView(-100, 0 * magicNegativeNumber);
-            }} className='project'>
-                <h2>Checkbook Pocket</h2>
-                <div className="projectLinks">
-                    <a href="https://github.com/CameronHardy9/CheckBook-Pocket" rel="noreferrer" target="_blank">GitHub Repo</a>
-                    <a href="https://checkbook-pocket.web.app" rel="noreferrer" target="_blank">Live Site</a>
-                </div>
-            </article>
-            <article onClick={() => {
+            }}>
+                Checkbook Pocket
+            </button>
+            <button className='projectNav' onClick={() => {
                 props.updateView(-200, 0 * magicNegativeNumber);
-            }} className='project'>
-                <h2>Kanban Go</h2>
-                <div className="projectLinks">
-                    <a href="https://github.com/CameronHardy9/Kanban_Go" rel="noreferrer" target="_blank">GitHub Repo</a>
-                    <a href="https://kanban-go.netlify.app/" rel="noreferrer" target="_blank">Live Site</a>
-                </div>
-            </article>
-            <article onClick={() => {
+            }}>
+                Kanban Go
+            </button>
+            <button className='projectNav' onClick={() => {
                 props.updateView(-400, 0 * magicNegativeNumber);
-            }} className='project'>
-                <h2>Pokedex</h2>
-                <div className="projectLinks">
-                    <a href="https://github.com/CameronHardy9/Pokedex_Project" rel="noreferrer" target="_blank">GitHub Repo</a>
-                    <a href="https://project-pokedex.netlify.app/" rel="noreferrer" target="_blank">Live Site</a>
-                </div>
-            </article>
+            }}>
+                Pokedex
+            </button>
         </section>
     );
 };
