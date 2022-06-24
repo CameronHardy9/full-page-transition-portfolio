4ffa118f20f601369038d9604b325cab9312469f
Cameron Hardy <camhardy99@gmail.com>
2022-06-24 16:02:50 -0400

    Added basic contact fields

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -3,7 +3,14 @@ import './Contact.css';
 export default function Contact(){
     return(
         <section className="contactContainer page red">
-            <h1>Contact</h1>
+            <label htmlFor="contactForm">Get in touch!</label>
+            <form id='contactForm'>
+                <input type="text" name="fullName" placeholder='Full Name' />
+                <input type="email" name="email" placeholder='Email' />
+                <input type="tel" name="phone" placeholder='Phone' />
+                <textarea name="message" cols="30" rows="5" placeholder='Message'></textarea>
+                <button type="submit">SEND</button>
+            </form>
         </section>
     );
 };
