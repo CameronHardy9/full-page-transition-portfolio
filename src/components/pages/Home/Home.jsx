22b2226a461d093fea9c03febac5b5a7599852dc
Cameron Hardy <camhardy99@gmail.com>
2022-07-06 11:45:17 -0400

    Refactored for more responsive design

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,20 +1,29 @@
 import './Home.css';
+import { useEffect } from 'react';
 
 export default function Home(){
+    useEffect(() => {
+        const height = document.getElementById('homeH1').clientHeight;
+        style.h1 = {
+            fontSize: `${height / 3}px`,
+            lineHeight: `${height / 3}px`
+        };
+    }, []);
+
     return(
         <section className="page grey">
-            <div className='homeContainer'>
-                <h1 className='homeH1'>
-                    <span>Hi</span>
-                    <span>I am</span>
-                    <span>Cameron</span>
-                </h1>
-                {/* <p className='homeP'>
-                    <span>Coffee Enthusiast</span>
-                    <span>Developer</span>
-                    <span>Maker</span>
-                </p> */}
-            </div>
+            <h1 id='homeH1'>
+                <span style={style.h1}>Hi</span>
+                <span style={style.h1}>I am</span>
+                <span style={style.h1}>Cameron</span>
+            </h1>
         </section>
     );
+};
+
+const style = {
+    h1: {
+        fontSize: '25vh',
+        lineHeight: 'normal'
+    }
 };
