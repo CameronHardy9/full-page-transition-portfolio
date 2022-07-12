48983fa701423eeae8fc3f4313d9080f351ff6ed
Cameron Hardy <camhardy99@gmail.com>
2022-07-11 21:18:52 -0500

    Added new project page coordinates to project nav

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -7,22 +7,22 @@ export default function Projects(props){
     return(
         <section className="projectsContainer page orange">
             <button className='projectNav' onClick={() => {
-                props.updateView(0, 0 * magicNegativeNumber);
+                props.updateView(-600, 1 * magicNegativeNumber);
             }}>
                 Portfolio Site
             </button>
             <button className='projectNav' onClick={() => {
-                props.updateView(-100, 0 * magicNegativeNumber);
+                props.updateView(-400, 2 * magicNegativeNumber);
             }}>
                 Checkbook Pocket
             </button>
             <button className='projectNav' onClick={() => {
-                props.updateView(-200, 0 * magicNegativeNumber);
+                props.updateView(-300, 6 * magicNegativeNumber);
             }}>
                 Kanban Go
             </button>
             <button className='projectNav' onClick={() => {
-                props.updateView(-400, 0 * magicNegativeNumber);
+                props.updateView(-200, 3 * magicNegativeNumber);
             }}>
                 Pokedex
             </button>
