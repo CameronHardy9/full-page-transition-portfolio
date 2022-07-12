2fc3f2ebae76d43cead05a518e4ea6cfa48cef30
Cameron Hardy <camhardy99@gmail.com>
2022-07-11 22:09:53 -0500

    Added back buttons and reformatted project pages

--- src/components/pages/Projects/projectPages/CheckbookPocket.jsx
+++ src/components/pages/Projects/projectPages/CheckbookPocket.jsx
@@ -1,19 +1,58 @@
-export default function CheckbookPocket(){
-    return(
+export default function CheckbookPocket(props) {
+    return (
         <article className="projectContainer page red">
+            <svg
+                onClick={() => {
+                    props.updateView(0, 5 * props.magicNegativeNumber)
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <line x1="19" y1="12" x2="5" y2="12"></line>
+                <polyline points="12 19 5 12 12 5"></polyline>
+            </svg>
             <h1>Checkbook Pocket</h1>
             <div className="projectPage">
-                <img src={require("../assets/CheckbookPocket.png")} alt="CheckbookPocket Project" />
+                <img
+                    src={require("../assets/CheckbookPocket.png")}
+                    alt="CheckbookPocket Project"
+                />
                 <div className="projectInfo">
                     <div className="projectLinks">
-                        <a href="https://github.com/CameronHardy9/CheckBook-Pocket" rel="noreferrer" target="_blank">GitHub Repo</a>
-                        <a href="https://checkbook-pocket.web.app" rel="noreferrer" target="_blank">Live Site</a>
+                        <a
+                            href="https://github.com/CameronHardy9/CheckBook-Pocket"
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            GitHub Repo
+                        </a>
+                        <a
+                            href="https://checkbook-pocket.web.app"
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            Live Site
+                        </a>
                     </div>
                     <p>
-                        Full-stack app using React on the frontend, Node/Express for the backend, and MongoDB for a database. Checkbook Pocket is a budget tracking app where users can set a budget and log their spending. Purchases are tracked by date and visualized in a list form where they can be added, edited, or deleted. Most UI/UX features were created using the Material UI library. User authentication and hosting is handled by Firebase.
+                        Full-stack app using React on the frontend, Node/Express
+                        for the backend, and MongoDB for a database. Checkbook
+                        Pocket is a budget tracking app where users can set a
+                        budget and log their spending. Purchases are tracked by
+                        date and visualized in a list form where they can be
+                        added, edited, or deleted. Most UI/UX features were
+                        created using the Material UI library. User
+                        authentication and hosting is handled by Firebase.
                     </p>
                 </div>
             </div>
         </article>
     );
-};
+}
--- src/components/pages/Projects/projectPages/KanbanGo.jsx
+++ src/components/pages/Projects/projectPages/KanbanGo.jsx
@@ -1,6 +1,23 @@
-export default function KanbanGo(){
+export default function KanbanGo(props){
     return(
         <article className="projectContainer page red">
+            <svg
+                onClick={() => {
+                    props.updateView(0, 5 * props.magicNegativeNumber)
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <line x1="19" y1="12" x2="5" y2="12"></line>
+                <polyline points="12 19 5 12 12 5"></polyline>
+            </svg>
             <h1>Kanban Go</h1>
             <div className="projectPage">
                 <img src={require("../assets/KanbanGo.png")} alt="KanbanGo Project" />
